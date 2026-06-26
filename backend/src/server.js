105746e993b597cb90b7efe1f67d3b982619eb32
Moriarty Puth <p.camboeav@gmail.com>
2026-06-26 23:14:56 +0700

    Configure: allow GitHub Pages domains in CORS origin whitelist

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,7 +26,12 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://moriartyputh.github.io',
+    'https://www.moriartyputh.github.io'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
